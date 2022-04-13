70a40ccfd4d0b20777b98ab83b9301c36ed3b48e
MatiasRomeroWasTaken <matiasromero@santandertecnologia.com.ar>
2022-04-13 18:04:39 -0300

    Agregando respuesta del ejercicio 3

---
+++ 2. Java/Ejercicio_3/src/test.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese cantidad de dias");
+
+        int dias= scanner.nextInt();
+
+        int resultado =dias*2;
+
+        System.out.println("Costo total de la campaña: \t"+"$"+resultado);
+
+    }
+}
