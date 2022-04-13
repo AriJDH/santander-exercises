0099fac695156eb89963edcdb48ae00db21a4bcd
MarianaRodriguez1 <marianarodriguez1@santandertecnologia.com.ar>
2022-04-13 18:32:14 -0300

    agregando ejercicio 1 de introduccion a Java

---
+++ src/Ejercicio1_C4.java
@@ -0,0 +1,11 @@
+public class Ejercicio1_C4 {
+    public static void main(String[] args) {
+
+        String apellido = "Gomez";    // faltan las comillas
+        int edad = 35;   //sobran las comillas
+        boolean b = false; //sobran las comillas y falta nombre de variable
+        double sueldo = 45857.90;   //error en tipo de dato, decia String y falta punto y coma
+        String nombre = "Julián";   //error en tipo de dato, decia double
+
+    }
+}
