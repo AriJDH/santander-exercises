39878d908fac6b99d9ede248cf0cfb464eb6b7a0
Venegas <A446455@santandertecnologia.com.ar>
2022-04-13 17:49:41 -0300

    Ejercicio1

---
+++ 2. Java/src/com/primerProyecto/Exercise1.java
@@ -0,0 +1,17 @@
+package com.primerProyecto;
+
+public class Exercise1 {
+
+    public static void main(String[] args) {
+
+// Ejercicio 1 (correccion de variables)
+
+        String apellido = "Gomez";
+        int edad = 35;
+        boolean criterio = false;
+        String nombre = "Julian";
+        double sueldo = 45857.90;
+
+
+    }
+}
