abfe89467edd32a55189d4e01b5f02820a1f3a36
AgustinLeguizamon-ST <agleguizamon@santandertecnologia.com.ar>
2022-04-13 17:23:53 -0300

    resuelto ejercicio 2

---
+++ 2. Java/src/com/bootcamp/Ejercicio2.java
@@ -0,0 +1,13 @@
+package com.bootcamp;
+
+public class Ejercicio2 {
+    public void ejecutar() {
+        String nombre;
+        int edad;
+        double sueldo;
+        String apellido;
+        String direccion;
+        boolean tiene_licencia_de_conducir;
+        double estatura_de_la_persona;
+    }
+}
--- 2. Java/src/com/bootcamp/Ejercicio3A.java
+++ 2. Java/src/com/bootcamp/Ejercicio3A.java
@@ -4,5 +4,8 @@ public class Ejercicio3A {
     public static void main(String[] args) {
         Ejercicio1 ejercicio1 = new Ejercicio1();
         ejercicio1.ejecutar();
+
+        Ejercicio2 ejercicio2 = new Ejercicio2();
+        ejercicio2.ejecutar();
     }
 }
