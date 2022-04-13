0e9f73f77f534a9bf209a53009780c0aa8c0b15b
deRivasLeandroSantander <lderivas@santandertecnologia.com.ar>
2022-04-13 18:15:15 -0300

    Se agregan las resoluciones del ejercicio 2

---
+++ 2. Java/Ejercicio2Java.java
@@ -0,0 +1,13 @@
+public class Ejercicio1Java {
+    public static void main(String[] args) {
+        // Ejercicio 2: Correcciones escritas al lado.
+        String 1nombre; //Incorrecto
+        int edad; //Correcto
+        double $ueldo; //Incorrecto
+        String @pellido; //Incorrecto
+        String direccion; //Correcto
+        boolean licencia_de_conducir; //Correcto
+        double estatura de la persona; //Incorrecto
+        int cantidad-de-hijos; //Incorrecto
+    }
+}
