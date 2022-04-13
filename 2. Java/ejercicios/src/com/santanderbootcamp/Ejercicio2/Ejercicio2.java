a5ea7fa0a1e7395a5c7978ab6eeed1a3c6022ffc
Casco <A446460@santandertecnologia.com.ar>
2022-04-13 17:29:58 -0300

    Ejercicio 2

---
+++ 2. Java/ejercicios/src/com/santanderbootcamp/Ejercicio2/Ejercicio2.java
@@ -0,0 +1,14 @@
+package com.santanderbootcamp.Ejercicio2;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        String nombre;                  //no es válido que una variable comience con un número
+        int edad;
+        double sueldo;                  //no es válido que el nombre comience con "$"
+        String apellido;                //no es válido que una variable comience con un símbolo
+        String direccion;
+        boolean licencia_de_conducir;
+        double estatura_de_la_persona;  //no es válido que el nombre tenga espacios
+        int cantidad_de_hijos;          //no es válido que el nombre contenga "-"
+    }
+}
