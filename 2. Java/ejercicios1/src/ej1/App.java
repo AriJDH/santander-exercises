1be9df249ca42d2e5c0824f72a3a7c7135f22e95
Juanil22 <juanilopez@santandertecnologia.com.ar>
2022-04-13 17:15:34 -0300

    ejercicio 1 subido

---
+++ 2. Java/ejercicios1/src/ej1/App.java
@@ -0,0 +1,9 @@
+package ej1;
+
+public class App {
+    String apellido = "Gomez";
+    int edad = 35;
+    boolean nombreLibre=false;
+    double sueldo = 45857.90;
+    String nombre = "Julián";
+}
