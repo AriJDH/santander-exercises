bb835bd2cdf010989ff0292302793cd4a667fcdc
Juanil22 <juanilopez@santandertecnologia.com.ar>
2022-04-13 17:19:44 -0300

    ej2

---
+++ 2. Java/ejercicios1/src/ej1/App2.java
@@ -0,0 +1,14 @@
+package ej1;
+
+public class App2 {
+    public static void main(String[] args) {
+        String nombre;
+        int edad;
+        double sueldo;
+        String apellido;
+        String direccion;
+        boolean licencia_de_conducir;
+        double estaturaPersona;
+        int cantidadHijos;
+    }
+}
