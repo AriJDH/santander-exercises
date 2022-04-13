3064215f90b581565b077d2ee937dead5277b251
Casco <A446460@santandertecnologia.com.ar>
2022-04-13 17:22:23 -0300

    Ejercicio 1

---
+++ 2. Java/ejercicios/src/com/santanderbootcamp/Ejercicio1/Ejercicio1.java
@@ -0,0 +1,11 @@
+package com.santanderbootcamp.Ejercicio1;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        String apellido = "Gomez";  //un String va entre ""
+        int edad = 35;              //un int no lleva ""
+        boolean unBoolean = false;  //le falta el nombre y un boolean no lleva ""
+        double sueldo = 45857.90;   //debería ser del tipo double y le falta ";" al final
+        String nombre = "Julián";   //debería ser del tipo String
+    }
+}
