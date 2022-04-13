7a4a3bca055b35688193a4cb87b7cd3da4f8ea05
gVersionFun <A446491@santandertecnologia.com.ar>
2022-04-13 17:23:05 -0300

    primeros dos ejercicios

---
+++ 2. Java/src/JavaEjercicios/Ejercicio.java
@@ -0,0 +1,25 @@
+package JavaEjercicios;
+
+public class Ejercicio {
+    public static void main(String[] args) {
+        //verificacion de variables
+
+        String apellido = "Gomez";
+        int edad = 35;
+        boolean falso = false;
+        double sueldo = 45857.90;
+        String nombre = "Julián";
+
+        //proponer nombres de variables
+        String nombre_estudiante;
+        int edad_estudiante;
+        double sueldo2;
+        String apellido2;
+        String direccion;
+        boolean licencia_de_conducir;
+        double estatura_de_la_persona;
+        int cantidad_de_hijos;
+
+        //
+    }
+}
