b967037a0cd4a3e53d866dfa32011300958d057d
Casco <A446460@santandertecnologia.com.ar>
2022-04-13 18:00:16 -0300

    Agregado Ejercicio4 con algunas fallas

---
+++ 2. Java/ejercicios/src/com/santanderbootcamp/Ejercicio4/Ejercicio4.java
@@ -0,0 +1,16 @@
+package com.santanderbootcamp.Ejercicio4;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        double premioBruto;
+        double premioFinal;
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Ingrese el monto bruto del premio: $");
+        premioBruto = entrada.nextDouble();
+        premioFinal = premioBruto - premioBruto * 0.0245 - premioBruto * 0.15 - premioBruto * 0.03;
+        System.out.println("El monto final que corresponde según los descuentos es $" + premioFinal);
+        entrada.close();
+    }
+}
