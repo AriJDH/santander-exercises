b0c27eb3c4cdda93cc8eab53093bee942881af66
goaguirre7 <goaguirre@santandertecnologia.com.ar>
2022-04-13 18:03:49 -0300

    M3C4 | Ejercicio 1

---
+++ 2. Java/DH-M3C4/src/com/santander/ejercicios/Main.java
@@ -0,0 +1,29 @@
+package com.santander.ejercicios;
+
+public class Main {
+    public static void main(String[] args) {
+        ejercicio1();
+    }
+
+    public static void ejercicio1() {
+        // Faltan las comillas
+        // String apellido = Gomez;
+        String apellido = "Gomez";
+
+        // El número no debe estar entre comillas (salvo cuando se requiera un String)
+        // int edad = "35";
+        int edad = 35;
+
+        // Falta el nombre de la variable
+        // boolean = "false";
+        boolean unaVariable = false;
+
+        // El valor asignado a sueldo es un double
+        // String sueldo = 45857.90
+        double sueldo = 45857.90;
+
+        // El valor asignado a nombre es un String
+        // double nombre = "Julián";
+        String nombre = "Julián";
+    }
+}
