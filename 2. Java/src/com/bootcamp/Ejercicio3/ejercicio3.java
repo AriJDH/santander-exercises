673cb22ac23762903bfbf143f70447a352774c85
mcartelli <mcartellifernandez@santandertecnologia.com.ar>
2022-04-13 17:36:58 -0300

    Terminado ejercicio 3

--- 2. Java/src/com/bootcamp/Ejercicio3/ejercicio3.java
+++ 2. Java/src/com/bootcamp/Ejercicio3/ejercicio3.java
@@ -1,4 +1,19 @@
 package com.bootcamp.Ejercicio3;
 
+import java.util.Scanner;
+
 public class ejercicio3 {
+    public static void main(String[] arg){
+
+        Scanner Teclado = new Scanner(System.in);
+
+        System.out.println("Número de días de campaña: ");
+
+        int diasCampaña = Teclado.nextInt();
+
+        int montoCampaña = 2 * diasCampaña;
+
+        System.out.println("El monto total es de: $" + montoCampaña);
+
+    }
 }
