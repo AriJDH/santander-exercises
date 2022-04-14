16fe792e6395ccb334bcbd786c5a9f53d2747dd1
vbeorda <vbeorda@santandertecnologia.com.ar>
2022-04-14 17:07:10 -0300

    exercise 2

---
+++ 2. Java/c4-exercises/src/com/santander/bootcamp/exercise2.java
@@ -0,0 +1,15 @@
+package com.santander.bootcamp;
+
+public class exercise2 {
+    public static void main(String[] args) {
+        String nombre;
+        int edad;
+        double sueldo;
+        String apellido;
+        String direccion;
+        boolean licencia_de_conducir;
+        double estaturaDeLaPersona;
+        int cantidad_de_hijos;
+        System.out.println("Ok");
+    }
+}
