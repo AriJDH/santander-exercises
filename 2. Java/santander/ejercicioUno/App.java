631f78dfe1a54e7df65a136859e28ee66a33d323
braiSantec <bcontrera@santandertecnologia.com.ar>
2022-04-18 10:07:21 -0300

    ejercicios

---
+++ 2. Java/santander/ejercicioUno/App.java
@@ -0,0 +1,17 @@
+package com.santander.ejercicioUno;
+
+public class App {
+
+    public static void main(String[] args) {
+        //Ejercicio 1
+        String apellido = "Gomez";
+        int edad = 35;
+        boolean bandera = false;
+        double sueldo = 45857.90;
+        String nombre = "Julián";
+
+        //Ejercicio2
+
+    }
+
+}
