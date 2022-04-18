4f24a4fd49ec11b771219ae2afe0ad426f65884b
goaguirre7 <goaguirre@santandertecnologia.com.ar>
2022-04-18 12:35:09 -0300

    M3C5P1 | Ejercicio 2

--- 2. Java/DH-M3C5P1/src/com/santander/sistema/Main.java
+++ 2. Java/DH-M3C5P1/src/com/santander/sistema/Main.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ejercicio1();
+        // ejercicio1();
+        // ejercicio2();
+        ejercicio3();
     }
 
     public static void ejercicio1() {
@@ -19,5 +21,30 @@ public class Main {
 
         double precio_final = precio_costo * (1 + envios[num_fabrica - 1]) * (1 + ganancia);
         System.out.printf("%nPrecio de venta: $%.2f%n", precio_final);
+    }
+
+    public static void ejercicio2() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("D.N.I.: ");
+        String dni = input.nextLine();
+        System.out.print("Sueldo: $");
+        double sueldo = input.nextDouble();
+        double aumento = 0.0;
+
+        if (sueldo <= 20_000) {
+            aumento = 0.20;
+        } else if (sueldo <= 45_000) {
+            aumento = 0.10;
+        } else {
+            aumento = 0.05;
+        }
+
+        System.out.printf("Porcentaje de aumento: %.2f%%%n", aumento * 100);
+        System.out.printf("Sueldo: $%.2f%n", sueldo * (1 + aumento));
+    }
+
+    public static void ejercicio3() {
+
     }
 }
