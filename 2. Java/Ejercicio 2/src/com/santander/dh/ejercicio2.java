366bbc91888eb4fa90081918d84d4dc06ffa0528
Tinti <A446463@santandertecnologia.com.ar>
2022-04-18 09:57:49 -0300

    Ejercicio 2

---
+++ 2. Java/Ejercicio 2/src/com/santander/dh/ejercicio2.java
@@ -0,0 +1,14 @@
+package com.santander.dh;
+
+public class ejercicio2 {
+    public static void main(String[] args) {
+        String nombre; //Corregido
+        int edad;
+        double sueldo; //Corregido
+        String apellido; //Corregido
+        String direccion;
+        boolean licenciaDeConducir; //Corregido
+        double estaturaDeLaPersona; //Corregido
+        int cantidadDeHijos; //Corregido
+    }
+}
