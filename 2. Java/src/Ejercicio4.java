e0ee96d730efcb1faaaa38ca65c116c32d8e547d
Sanchez <A446494@santandertecnologia.com.ar>
2022-04-18 15:19:07 -0300

    Correccion ejercicio

--- 2. Java/src/Ejercicio4.java
+++ 2. Java/src/Ejercicio4.java
@@ -4,18 +4,20 @@ public class Ejercicio4 {
     public static void main(String[] args) {
         Scanner scann = new Scanner(System.in);
         int contadorDiarioA = 0, contadorDiarioB = 0, contadorDiarioC = 0;
-
         for (int i = 1; i <= 50; i++) {
-            System.out.println("Ingresar a que diario está suscripto el encuestado " + i + " siendo: A, B o C las posibilidades");
-            String diario = scann.nextLine();
-            switch (diario){
-                case "A":
-                    contadorDiarioA++;
-                case "B":
-                    contadorDiarioB++;
-                case "C":
-                    contadorDiarioC++;
+            System.out.println("¿Está suscripto al diario A? (S/N)");
+            if (scann.nextLine().equals("S")){
+                contadorDiarioA++;
             }
+            System.out.println("¿Está suscripto al diario B? (S/N)");
+            if (scann.nextLine().equals("S")){
+                contadorDiarioB++;
+            }
+            System.out.println("¿Está suscripto al diario C? (S/N)");
+            if (scann.nextLine().equals("S")){
+                contadorDiarioC++;
+            }
+
         }
         System.out.println("La cantidad de personas que estan suscriptas a los tres diarios son: " + (contadorDiarioA + contadorDiarioB + contadorDiarioC));
         System.out.println("La cantidad de personas que estan suscriptas al diario A y C son: " + (contadorDiarioA + contadorDiarioC));
