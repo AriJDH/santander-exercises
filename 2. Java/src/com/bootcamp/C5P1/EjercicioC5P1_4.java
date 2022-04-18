ba79eb679c0445d9650c8698fca833ab842d6dc8
AgustinLeguizamon-ST <agleguizamon@santandertecnologia.com.ar>
2022-04-18 13:19:35 -0300

    aclarado nombre de variable

--- 2. Java/src/com/bootcamp/C5P1/EjercicioC5P1_4.java
+++ 2. Java/src/com/bootcamp/C5P1/EjercicioC5P1_4.java
@@ -1,6 +1,5 @@
 package com.bootcamp.C5P1;
 
-import java.util.Locale;
 import java.util.Scanner;
 
 public class EjercicioC5P1_4 {
@@ -9,7 +8,7 @@ public class EjercicioC5P1_4 {
     public void ejecutar() {
         Scanner teclado = new Scanner(System.in);
         String entrada = "";
-        int suscriptos_en_A_B_C = 0, suscriptos_en_A_C = 0, suscriptos_en_A_B = 0, suscriptos_en_B_C = 0, suscriptos_en_A = 0;
+        int suscriptos_en_A_B_C = 0, suscriptos_en_A_C = 0, suscriptos_en_A_B = 0, suscriptos_en_B_C = 0, suscriptos_solo_en_A = 0;
 
         for (int i = 0; i < CANTIDAD_ENCUESTADOS; i++) {
             System.out.print("Ingrese a que diarios esta suscripto el usuario Nro " + i + " (ejemplo: ABC para indicar que esta suscripto a los 3) : ");
@@ -33,7 +32,7 @@ public class EjercicioC5P1_4 {
                 suscriptos_en_B_C++;
             }
             if(suscripto_en_A && !suscripto_en_B && !suscripto_en_C){
-                suscriptos_en_A++;
+                suscriptos_solo_en_A++;
             }
 
         }
@@ -42,6 +41,6 @@ public class EjercicioC5P1_4 {
         System.out.println("¿Cuántos están suscriptos al Diario A y al Diario C?: " + suscriptos_en_A_C);
         System.out.println("¿Cuántos están suscriptos al Diario A y al Diario B?: " + suscriptos_en_A_B);
         System.out.println("¿Cuántos están suscriptos al Diario B y al Diario C?: " + suscriptos_en_B_C);
-        System.out.println("¿Cuántos están suscriptos sólo al Diario A?: " + suscriptos_en_A);
+        System.out.println("¿Cuántos están suscriptos sólo al Diario A?: " + suscriptos_solo_en_A);
     }
 }
