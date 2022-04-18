3d18b78ca8f556200bfdaff9c4c1e95dd5599afb
rubencasco <rcasco@santandertecnologia.com.ar>
2022-04-18 13:01:22 -0300

    agregado ejercicio 3 estructuras de control

---
+++ 2. Java/ejercicios/src/com/santanderbootcamp/EstructurasDeControl/Ejercicio3.java
@@ -0,0 +1,27 @@
+package com.santanderbootcamp.EstructurasDeControl;
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int conCamaras = 7;
+        int conPatrullaje;
+        double costo;
+        System.out.print("Ingrese la cantidad de clientes tienen servicio de patrullaje: ");
+        conPatrullaje = teclado.nextInt();
+        for(int i = 1; i <= conCamaras; i++) {
+            costo = 1500.0;
+            if (conPatrullaje != 0) {
+                costo += 700.0;
+                conPatrullaje--;
+            }
+            if ((int)costo == 1500) {
+                System.out.printf("El cliente número %d tiene contratado el Servicio 1 con un costo de %.2f\n", i, costo);
+            }
+            else {
+                System.out.printf("El cliente número %d tiene contratado el Servicio 2 con un costo de %.2f\n", i, costo);
+            }
+        }
+    }
+}
