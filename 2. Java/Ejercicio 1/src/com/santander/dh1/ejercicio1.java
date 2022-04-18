9fc8dd03c61dda8988fa5c150358f985ece0056f
Tinti <A446463@santandertecnologia.com.ar>
2022-04-18 09:51:35 -0300

    Ejercicio 1

---
+++ 2. Java/Ejercicio 1/src/com/santander/dh1/ejercicio1.java
@@ -0,0 +1,15 @@
+package com.santander.dh1;
+
+public class ejercicio1 {
+    public static void main(String[] args) {
+        String apellido = "Gomez"; //Corregido
+        int edad = 35; //Corregido
+        boolean variable = false; //Corregido
+        double sueldo = 45857.90; //Corregido
+        String nombre = "Julian"; //Corregido
+
+
+    }
+
+
+}
