f8634574d2301b9c57aa15221b7fa1492052d535
Juanil22 <juanilopez@santandertecnologia.com.ar>
2022-04-18 17:53:11 -0300

    refactor ej

--- 2. Java/ejVectoresMatrices/src/com/ej1/App.java
+++ 2. Java/ejVectoresMatrices/src/com/ej1/App.java
@@ -8,43 +8,42 @@ public class App {
         Scanner teclado = new Scanner(System.in);
         System.out.print("Ingrese limite de ciudades a ingresar: ");
         int limite = teclado.nextInt();
-        String ciudades[]=new String[limite];
+        String ciudades[] = new String[limite];
 
         for (int i = 0; i < ciudades.length; i++) {
             System.out.print("ciudad: ");
-            ciudades[i]=teclado.next();
+            ciudades[i] = teclado.next();
         }
 
-        int temps[][]= new int[limite][2];
+        int temps[][] = new int[limite][2];
 
         for (int i = 0; i < temps.length; i++) {
-            System.out.print("ingrese mínima temp de "+ ciudades[i] + ":");
-            temps[i][0]=teclado.nextInt();
-            System.out.print("ingrese máxima temp de "+ ciudades[i] + ":");
-            temps[i][1]=teclado.nextInt();
+            System.out.print("ingrese mínima temp de " + ciudades[i] + ":");
+            temps[i][0] = teclado.nextInt();
+            System.out.print("ingrese máxima temp de " + ciudades[i] + ":");
+            temps[i][1] = teclado.nextInt();
         }
 
         //una vez cargado..
 
-        int tempMin= temps[0][0];
-        int tempMax=temps[0][1];
-        String ciudadMinima=ciudades[0];
-        String ciudadMaxima=ciudades[0];
+        int tempMin = temps[0][0];
+        int tempMax = temps[0][1];
+        String ciudadMinima = ciudades[0];
+        String ciudadMaxima = ciudades[0];
 
         for (int i = 0; i < limite; i++) {
-           if (temps[i][0]<tempMin){
-               tempMin=temps[i][0];
-               ciudadMinima=ciudades[i];
-           }
-        }
-        for (int i = 0; i < limite; i++) {
-            if (temps[i][1]>tempMax){
-                tempMax=temps[i][1];
-                ciudadMaxima=ciudades[i];
+            if (temps[i][0] < tempMin) {
+                tempMin = temps[i][0];
+                ciudadMinima = ciudades[i];
+            }
+            if (temps[i][1] > tempMax) {
+                tempMax = temps[i][1];
+                ciudadMaxima = ciudades[i];
             }
         }
-        System.out.println("la ciudad con temp min de "+ tempMin +"° es "+ ciudadMinima);
-        System.out.println("la ciudad con temp max de "+ tempMax +"° es "+ ciudadMaxima);
+
+        System.out.println("la ciudad con temp min de " + tempMin + "° es " + ciudadMinima);
+        System.out.println("la ciudad con temp max de " + tempMax + "° es " + ciudadMaxima);
     }
 
 }
