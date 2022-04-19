5d7cb4c4d4d12ea8a67ce13ae470ff8c385f61c6
rubencasco <rcasco@santandertecnologia.com.ar>
2022-04-19 14:56:23 -0300

    resuelto el ejercicio de estructuras dinamicas

--- 2. Java/ejercicios/src/com/santanderbootcamp/EstructurasDinamicas/Ejercicio.java
+++ 2. Java/ejercicios/src/com/santanderbootcamp/EstructurasDinamicas/Ejercicio.java
@@ -3,8 +3,8 @@ package com.santanderbootcamp.EstructurasDinamicas;
 import java.util.*;
 
 class Prenda {
-    private String marca;
-    private String modelo;
+    private final String marca;
+    private final String modelo;
     Prenda(String marca, String modelo) {
         this.marca = marca;
         this.modelo = modelo;
@@ -30,17 +30,21 @@ class Guardarropa {
     }
 
     public void mostrarPrendas() {
-        for(Map.Entry<Integer, List<Prenda>> listaDePrenda: dicPrendas.entrySet()) {
-            System.out.println("En el lugar " + listaDePrenda.getKey() + " están guardadas las prendas: ");
-            for(Prenda prenda: listaDePrenda.getValue()) {
-                System.out.println("->  "+ prenda.marca() + "  -  " + prenda.modelo());
+        if (dicPrendas.isEmpty()) {
+            System.out.println("No hay prendas.");
+        }
+        else {
+            for (Map.Entry<Integer, List<Prenda>> listaDePrenda : dicPrendas.entrySet()) {
+                System.out.println("En el lugar " + listaDePrenda.getKey() + " están guardadas las prendas: ");
+                for (Prenda prenda : listaDePrenda.getValue()) {
+                    System.out.println("->  " + prenda.marca() + "  -  " + prenda.modelo());
+                }
             }
         }
     }
     public List<Prenda> devolverPrendas(Integer numero){
         List<Prenda> prenda = dicPrendas.get(numero);
         dicPrendas.remove(numero);
-        System.out.println("Se retiraron las prendas.");
         return prenda;
     }
 }
@@ -48,18 +52,18 @@ class Guardarropa {
 public class Ejercicio {
     public static void main(String[] args) {
         Guardarropa guardaRopa = new Guardarropa();
-        Prenda prenda1 = new Prenda("Nike", "Zapatillas");
-        Prenda prenda2 = new Prenda("Adidas", "Buzo");
+        int codigo;
         List<Prenda> listaPrendas = new ArrayList<>();
-        listaPrendas.add(prenda1);
-        listaPrendas.add(prenda2);
-        int codigo = guardaRopa.guardarPrendas(listaPrendas);
+        listaPrendas.add(new Prenda("Nike", "Zapatillas"));
+        listaPrendas.add(new Prenda("Adidas", "Buzo"));
+
+        codigo = guardaRopa.guardarPrendas(listaPrendas);
         guardaRopa.mostrarPrendas();
         System.out.println();
         List<Prenda> listaPrenda2 = guardaRopa.devolverPrendas(codigo);
         guardaRopa.mostrarPrendas();
 
-        //solo para verificar que se hayan extraido correctamente
+        //para verificar que se hayan extraido correctamente
         System.out.println("\nLas prendas extraídas fueron:");
         for(Prenda prenda: listaPrenda2) {
             System.out.println("--> " + prenda.marca() + " - " + prenda.modelo());
