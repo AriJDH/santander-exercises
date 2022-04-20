6e7a5c48c6caa4ae1f55daf4796d88a8643d1338
rubencasco <rcasco@santandertecnologia.com.ar>
2022-04-20 17:35:37 -0300

    resuelto ejercicio 1 de excepciones y clases utilitarias

---
+++ 2. Java/ejercicios/src/com/santanderbootcamp/ExcepcionesYClasesUtilitarias/Ejercicio1/PracticaExcepciones.java
@@ -0,0 +1,23 @@
+package com.santanderbootcamp.ExcepcionesYClasesUtilitarias.Ejercicio1;
+
+public class PracticaExcepciones {
+    static int a = 0, b = 300;
+    public static int dividir(int a, int b) throws IllegalArgumentException {
+        if (b == 0) throw new IllegalArgumentException("No se puede dividir por cero");
+        return a/b;
+    }
+
+    public static void main(String[] args) {
+        int r;
+
+        try {
+            //r = b / a;
+            dividir(b,a);
+       // } catch (Exception e) {
+        } catch (IllegalArgumentException e) {
+            //System.out.println("Se ha producido un error");
+            System.out.println(e);
+        }
+        System.out.println("Programa finalizado");
+    }
+}
