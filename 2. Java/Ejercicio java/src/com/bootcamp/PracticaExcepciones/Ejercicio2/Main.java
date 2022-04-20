ad9cf173041340a1e158c89a4085a2069b69897a
mcartelli <mcartellifernandez@santandertecnologia.com.ar>
2022-04-20 18:14:08 -0300

    Terminado Ejercicio 3

--- 2. Java/Ejercicio java/src/com/bootcamp/PracticaExcepciones/Ejercicio2/Main.java
+++ 2. Java/Ejercicio java/src/com/bootcamp/PracticaExcepciones/Ejercicio2/Main.java
@@ -1,5 +1,7 @@
 package com.bootcamp.PracticaExcepciones.Ejercicio2;
 
+import com.bootcamp.PracticaExcepciones.Ejercicio3;
+
 public class Main {
     public static void main(String[] args) {
         String mensajeFinal = "Este es el último mensaje";
