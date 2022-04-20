4bcfdc1594935d8e1fee0656c56e89be50971f7d
rubencasco <rcasco@santandertecnologia.com.ar>
2022-04-20 13:30:08 -0300

    resuelto el ejercicio 6 de programacion orientada a objetos

--- 2. Java/ejercicios/src/com/santanderbootcamp/POO/Main.java
+++ 2. Java/ejercicios/src/com/santanderbootcamp/POO/Main.java
@@ -8,5 +8,23 @@ public class Main {
         Persona personaPepito = new Persona("12950012", "Pepito", 70);
         Persona personaJulian = new Persona("32334234", "Julian", 34, 72.0, 1.82);
 
+        System.out.println("La persona " + personaJulian + ": ");
+        System.out.print("--> Tiene un indice de masa corporal que se encuentra en: ");
+        switch (personaJulian.calcularIMC()) {
+            case -1:
+                System.out.println("Bajo Peso");
+                break;
+            case 0:
+                System.out.println("Peso Saludable");
+                break;
+            case 1:
+                System.out.println("Sobrepeso");
+        }
+        if (personaJulian.esMayorDeEdad()) {
+            System.out.println("--> Es mayor de edad");
+        }
+        else {
+            System.out.println("--> Es menor de edad");
+        }
     }
 }
