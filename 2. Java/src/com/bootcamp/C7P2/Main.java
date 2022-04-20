bb6b5cd30116b64fb30ea74eb158a736a4179e8f
AgustinLeguizamon-ST <agleguizamon@santandertecnologia.com.ar>
2022-04-20 15:52:13 -0300

    agregado ejercicio 3

--- 2. Java/src/com/bootcamp/C7P2/Main.java
+++ 2. Java/src/com/bootcamp/C7P2/Main.java
@@ -1,9 +1,35 @@
 package com.bootcamp.C7P2;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         //ejercicio_1();
-        ejercicio_2();
+        //ejercicio_2();
+
+        /*ejercicio 3*/
+        leerArchivo("nombre random");
+    }
+
+    private static void leerArchivo(String nombreArchivo) {
+        String texto = null;
+
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader((nombreArchivo)));
+            while((texto = bufferedReader.readLine()) != null) {
+                System.out.println(texto);
+            }
+            bufferedReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("No se puede encontrar el archivo especificado");
+        } catch (IOException e) {
+            System.out.println("Error al leer el archivo especificado");
+        }
+
+
     }
 
     private static void ejercicio_2() {
