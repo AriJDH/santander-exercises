c5fcbe61c61ab911837905d01f95ec5452c06209
gVersionFun <A446491@santandertecnologia.com.ar>
2022-04-20 12:49:14 -0300

    Especificaciones

--- 2. Java/POO/src/PracticaIntegradora/Main.java
+++ 2. Java/POO/src/PracticaIntegradora/Main.java
@@ -9,6 +9,8 @@ public class Main {
         Persona personaCOnstructor3= new Persona("Carlos", "9424512", 18, 25.5, 1.75);
 
        //Presento la información
+
+        //IMC
         if(personaCOnstructor3.calcularIMC(personaCOnstructor3.altura, personaCOnstructor3.peso) == -1){
             System.out.println("Tiene muy bajo peso");
         }else if(personaCOnstructor3.calcularIMC(personaCOnstructor3.altura, personaCOnstructor3.peso) == 0){
@@ -16,6 +18,7 @@ public class Main {
         }else{
             System.out.println("Tiene sobrepeso");
         }
+        //Si es mayor de edad
         if(personaCOnstructor3.esMayorDeEdad(personaCOnstructor3.edad)){
             System.out.println("Usted es mayor de edad");
         }else{
