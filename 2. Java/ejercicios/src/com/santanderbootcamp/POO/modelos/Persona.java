6bb09e9f5fc5746fd2eab84bfe6975b2af1a1fd7
rubencasco <rcasco@santandertecnologia.com.ar>
2022-04-20 12:29:23 -0300

    resuelto el ejercicio 2 de programacion orientada a objetos

--- 2. Java/ejercicios/src/com/santanderbootcamp/POO/modelos/Persona.java
+++ 2. Java/ejercicios/src/com/santanderbootcamp/POO/modelos/Persona.java
@@ -6,9 +6,20 @@ public class Persona {
     private int edad;
     private double peso;
     private double altura;
+    
+    public Persona() {}
 
-    //nombre seria un String porque contiene una cadena de caracteres
-    //edad un int porque almacena un número entero
-    //peso y altura son double porque almacenan números que no son enteros
+    public Persona(String dni, String nombre, int edad) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.edad = edad;
+    }
 
+    public Persona(String dni, String nombre, int edad, double peso, double altura) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.peso = peso;
+        this.altura = altura;
+    }
 }
