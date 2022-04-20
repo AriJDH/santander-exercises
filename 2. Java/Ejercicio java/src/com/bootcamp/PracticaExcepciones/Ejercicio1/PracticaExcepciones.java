2d6613fb3763bae9ab7d8f66608fe740b0288b2d
mcartelli <mcartellifernandez@santandertecnologia.com.ar>
2022-04-20 17:44:38 -0300

    Terminado ejercicio 2 de excepciones

--- 2. Java/Ejercicio java/src/com/bootcamp/PracticaExcepciones/Ejercicio1/PracticaExcepciones.java
+++ 2. Java/Ejercicio java/src/com/bootcamp/PracticaExcepciones/Ejercicio1/PracticaExcepciones.java
@@ -1,22 +1,20 @@
 package com.bootcamp.PracticaExcepciones.Ejercicio1;
 
 public class PracticaExcepciones {
+    private static int b = 5;
+    private static int a = 0;
+
     public static void main(String[] args) {
-        int a = 300;
-        int b = 0;
+        dividir();
+    }
 
-
+    public static void dividir() {
         try {
-            if(b==0){
-                throw new IllegalArgumentException("No se puede dividir por 0");
-            }else{
-                int c = a/b;
-            }
-
-        }catch(IllegalArgumentException e){
+            if (a == 0)
+                throw new IllegalArgumentException("No se puede dividir por cero");
+        } catch (IllegalArgumentException e) {
             e.printStackTrace();
-        }finally {
-            System.out.println("Programa Finalizado");
         }
+
     }
 }
