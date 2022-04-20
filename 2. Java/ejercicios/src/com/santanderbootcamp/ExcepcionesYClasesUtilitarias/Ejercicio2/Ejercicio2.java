8bb57ae92593a9bb980a5f196af6205084a4e815
rubencasco <rcasco@santandertecnologia.com.ar>
2022-04-20 17:46:30 -0300

    resuelto ejercicio 2 de excepciones y clases utilitarias

---
+++ 2. Java/ejercicios/src/com/santanderbootcamp/ExcepcionesYClasesUtilitarias/Ejercicio2/Ejercicio2.java
@@ -0,0 +1,15 @@
+package com.santanderbootcamp.ExcepcionesYClasesUtilitarias.Ejercicio2;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        String mensajeFinal = "Este es el último mensaje";
+        int[] numeros = new int[5];
+        try {
+            numeros[5] = 10;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e);
+        } finally {
+            System.out.println(mensajeFinal);
+        }
+    }
+}
