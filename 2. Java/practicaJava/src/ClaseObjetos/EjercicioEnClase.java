bb5afd01440dbb30086ff79ebd455d2e2b534762
hersantander <herromero@santandertecnologia.com.ar>
2022-04-20 10:18:10 -0300

    corredor,circuito, inscripciones

---
+++ 2. Java/practicaJava/src/ClaseObjetos/EjercicioEnClase.java
@@ -0,0 +1,4 @@
+package ClaseObjetos;
+
+public class EjercicioEnClase {
+}
--- 2. Java/practicaJava/src/estructurasDinamicas/Main.java
+++ 2. Java/practicaJava/src/estructurasDinamicas/Main.java
@@ -6,12 +6,31 @@ public class Main {
         Prenda unaPrenda= new Prenda("nike", "44");
         Prenda otraPrenda= new Prenda("adidas", "35");
         Prenda otraMasPrenda= new Prenda("puma", "40");
-        GuardaRopa listaDeRopa = new GuardaRopa();
+        GuardaRopa guardaRopa = new GuardaRopa();
         ArrayList<Prenda> prendas = new ArrayList<>();
         prendas.add(unaPrenda);
         prendas.add(otraPrenda);
         prendas.add(otraMasPrenda);
-        listaDeRopa.guardarPrendas(prendas);
-        listaDeRopa.mostrarPrendas();
+        System.out.println("se guardan prendas del 1");
+        guardaRopa.guardarPrendas(prendas);
+
+        ArrayList<Prenda> prendas2 = new ArrayList<>();
+        prendas2.add(otraPrenda);
+        System.out.println("se guardan prendas del 2");
+        guardaRopa.guardarPrendas(prendas2);
+
+        ArrayList<Prenda> prendas3 = new ArrayList<>();
+        prendas3.add(otraMasPrenda);
+        System.out.println("se guardan prendas del 3");
+        guardaRopa.guardarPrendas(prendas3);
+
+        System.out.println("Mostramos guardarropas");
+
+        guardaRopa.mostrarPrendas();
+        guardaRopa.devolverPrendas(2);
+        System.out.println("El numero 2 devuelve prendas");
+        System.out.println("Mostramos guardarropas");
+
+        guardaRopa.mostrarPrendas();
     }
 }
