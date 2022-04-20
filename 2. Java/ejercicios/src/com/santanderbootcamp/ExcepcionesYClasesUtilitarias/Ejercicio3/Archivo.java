8be995b64476e784641f4a2f1dfac6efb4cd1abd
rubencasco <rcasco@santandertecnologia.com.ar>
2022-04-20 17:58:51 -0300

    resuelto ejercicio 3 de excepciones y clases utilitarias

---
+++ 2. Java/ejercicios/src/com/santanderbootcamp/ExcepcionesYClasesUtilitarias/Ejercicio3/Archivo.java
@@ -0,0 +1,27 @@
+package com.santanderbootcamp.ExcepcionesYClasesUtilitarias.Ejercicio3;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Archivo {
+    public void leerArchivo(String nombreArchivo) {
+        String texto = null;
+
+        BufferedReader bufferedReader = null;
+        try {
+            bufferedReader = new BufferedReader(new FileReader(nombreArchivo));
+            try {
+                while((texto = bufferedReader.readLine()) != null) {
+                    System.out.println(texto);
+                }
+                bufferedReader.close();
+            } catch (IOException e) {
+                System.out.println("Error al leer el archivo especificado");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("No se puede encontrar el archivo especificado");
+        }
+    }
+}
---
+++ 2. Java/ejercicios/src/com/santanderbootcamp/ExcepcionesYClasesUtilitarias/Ejercicio3/Ejercicio3.java
@@ -0,0 +1,9 @@
+package com.santanderbootcamp.ExcepcionesYClasesUtilitarias.Ejercicio3;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        Archivo archivo = new Archivo();
+
+        archivo.leerArchivo("txt");
+    }
+}
