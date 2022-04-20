b3298704a19d108cf47ab5f91ec5512ce3420287
AgustinLeguizamon-ST <agleguizamon@santandertecnologia.com.ar>
2022-04-20 11:34:53 -0300

    agregado ejercicio 4

--- 2. Java/src/com/bootcamp/C7P1/Main.java
+++ 2. Java/src/com/bootcamp/C7P1/Main.java
@@ -4,6 +4,11 @@ import com.bootcamp.C7P1.modelos.Persona;
 
 public class Main {
     public static void main(String[] args) {
-        Persona persona = new Persona("Agus",18,"38998262",78,1.84f);
+        Persona persona_completa = new Persona("Agus",18,"38998262",78,1.84f);
+        Persona persona_medio = new Persona("Agus",18,"38998262");
+        Persona persona_vacia = new Persona();
+        
+        //Error
+        //Persona persona_vacia = new Persona("agus", "18");
     }
 }
