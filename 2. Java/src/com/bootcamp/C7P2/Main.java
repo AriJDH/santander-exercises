38d42e42cf0b7a3826f3563735a0634ca298adc6
AgustinLeguizamon-ST <agleguizamon@santandertecnologia.com.ar>
2022-04-20 15:43:04 -0300

    agregado ejercicio 2

--- 2. Java/src/com/bootcamp/C7P2/Main.java
+++ 2. Java/src/com/bootcamp/C7P2/Main.java
@@ -2,9 +2,28 @@ package com.bootcamp.C7P2;
 
 public class Main {
     public static void main(String[] args) {
+        //ejercicio_1();
+        ejercicio_2();
+    }
+
+    private static void ejercicio_2() {
+        //Mensaje final
+        String mensajeFinal = "Este es el último mensaje";
+
+        //Código que arroja excepción
+        int[] numeros = new int[5];
+        try {
+            numeros[5] = 10;
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println(e);
+        }
+
+        System.out.println(mensajeFinal);
+    }
+
+    public static void ejercicio_1() {
         PracticaExcepciones practicaExcepciones = new PracticaExcepciones();
         practicaExcepciones.ejecutar();
         practicaExcepciones.ejecutar_con_excepcion();
-
     }
 }
