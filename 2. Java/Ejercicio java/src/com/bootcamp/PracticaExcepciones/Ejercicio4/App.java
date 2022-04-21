15ce13efe9ee2e9b1b770920a7d5e0d96cb4c650
mcartelli <mcartellifernandez@santandertecnologia.com.ar>
2022-04-21 10:13:23 -0300

    Cambie los metodos del ejercicio 4 a static para no necesitar crear una instancia

--- 2. Java/Ejercicio java/src/com/bootcamp/PracticaExcepciones/Ejercicio4/App.java
+++ 2. Java/Ejercicio java/src/com/bootcamp/PracticaExcepciones/Ejercicio4/App.java
@@ -2,11 +2,10 @@ package com.bootcamp.PracticaExcepciones.Ejercicio4;
 
 public class App {
     public static void main(String[] args) {
-        Calculos calculo = new Calculos();
 
-        System.out.println(calculo.Random());
-        System.out.println(calculo.Max(5523,121));
-        System.out.println(calculo.RaizC(123.23));
-        System.out.println(calculo.Exponent(33,23.2));
+        System.out.println(Calculos.Random());
+        System.out.println(Calculos.Max(5523,121));
+        System.out.println(Calculos.RaizC(123.23));
+        System.out.println(Calculos.Exponent(33,23.2));
     }
 }
--- 2. Java/Ejercicio java/src/com/bootcamp/PracticaExcepciones/Ejercicio4/Calculos.java
+++ 2. Java/Ejercicio java/src/com/bootcamp/PracticaExcepciones/Ejercicio4/Calculos.java
@@ -1,23 +1,23 @@
 package com.bootcamp.PracticaExcepciones.Ejercicio4;
 
-public class Calculos {
-
-    public double Max(double a,double b){
+public  class Calculos {
+    // el static hace que yo pueda llamar a la clase sin crear una instancia :)
+    public static double Max(double a,double b){
         return Math.max(a,b);
     }
-    public double Min(double a,double b){
+    public static double Min(double a,double b){
         return Math.min(a,b);
     }
-    public double Exponent(double a,double exponent){
+    public static double Exponent(double a,double exponent){
         return Math.pow(a,exponent);
     }
-    public float Cos(double a){
+    public static float Cos(double a){
         return (float) Math.cos(a);
     }
-    public float RaizC(double a){
+    public static float RaizC(double a){
         return (float) Math.pow(a,0.5f);
     }
-    public float Random(){
+    public static float Random(){
         return (float) Math.random()*999;
     }
 
