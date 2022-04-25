ec1ca2cd924a672e04eda5cad836056954bac755
mcartelli <mcartellifernandez@santandertecnologia.com.ar>
2022-04-25 15:53:17 -0300

    comienzo ejercicio Morse C11P1

---
+++ 3. Spring/C11P1/Morse/src/test/java/com/C11P1/Morse/MorseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.C11P1.Morse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MorseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
