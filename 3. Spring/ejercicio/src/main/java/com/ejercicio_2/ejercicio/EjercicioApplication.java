242f5b7a368f9d25349e34ce809e20a0d5a40ef2
AgustinLeguizamon-ST <agleguizamon@santandertecnologia.com.ar>
2022-04-25 11:52:56 -0300

    agregado ejercicio maven y ejercicio spring boot

---
+++ 3. Spring/ejercicio/src/main/java/com/ejercicio_2/ejercicio/EjercicioApplication.java
@@ -0,0 +1,23 @@
+package com.ejercicio_2.ejercicio;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioApplication implements CommandLineRunner {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(EjercicioApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		//System.out.println(" Hola Spring");
+		LOGGER.info("Hola Spring");
+	}
+}
---
+++ 3. Spring/ejercicio/src/test/java/com/ejercicio_2/ejercicio/EjercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejercicio_2.ejercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
