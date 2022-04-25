302582e8da05666bbd03c0db942baf195f3ffd57
AgustinLeguizamon-ST <agleguizamon@santandertecnologia.com.ar>
2022-04-25 16:07:13 -0300

    fix convertidor no convertia bien algunos numeros

--- 3. Spring/ejercicio/src/main/java/com/ejercicio_2/ejercicio/model/Convertidor.java
+++ 3. Spring/ejercicio/src/main/java/com/ejercicio_2/ejercicio/model/Convertidor.java
@@ -5,6 +5,9 @@ import java.util.HashMap;
 public class Convertidor {
 
     private final HashMap<String, Integer> ROMANOS_A_DECIMAL = new HashMap<>();
+    private final int I_PRIMER_NUMERO = 0;
+    private final int I_SEGUNDO_NUMERO = 1;
+    private final int LETRAS_CONSECUTIVAS = 2;
 
     public Convertidor() {
         ROMANOS_A_DECIMAL.put("I", 1);
@@ -33,23 +36,43 @@ public class Convertidor {
         String letra_romana = "";
 
         while (resto > 0){
-            if (resto >= ROMANOS_A_DECIMAL.get("M")){
+            if (resto >= ROMANOS_A_DECIMAL.get("M")) {
                 letra_romana = "M";
+            } else if (resto >= ROMANOS_A_DECIMAL.get("M") - ROMANOS_A_DECIMAL.get("C")){
+                letra_romana = "CM";
             } else if (resto >= ROMANOS_A_DECIMAL.get("D")){
                 letra_romana = "D";
+            } else if (resto >= ROMANOS_A_DECIMAL.get("D") - ROMANOS_A_DECIMAL.get("C")){
+                letra_romana = "CD";
             } else if (resto >= ROMANOS_A_DECIMAL.get("C")){
                 letra_romana = "C";
+            } else if (resto >= ROMANOS_A_DECIMAL.get("C") - ROMANOS_A_DECIMAL.get("X")){
+                letra_romana = "XC";
             } else if (resto >= ROMANOS_A_DECIMAL.get("L")){
                 letra_romana = "L";
+            } else if (resto >= ROMANOS_A_DECIMAL.get("L") - ROMANOS_A_DECIMAL.get("X")){
+                letra_romana = "XL";
             } else if (resto >= ROMANOS_A_DECIMAL.get("X")){
                 letra_romana = "X";
+            } else if (resto >= ROMANOS_A_DECIMAL.get("X") - ROMANOS_A_DECIMAL.get("I")){
+                letra_romana = "IX";
             } else if (resto >= ROMANOS_A_DECIMAL.get("V")){
                 letra_romana = "V";
+            } else if (resto >= ROMANOS_A_DECIMAL.get("V") - ROMANOS_A_DECIMAL.get("I")){
+                letra_romana = "IV";
             } else if (resto >= ROMANOS_A_DECIMAL.get("I")){
                 letra_romana = "I";
             }
             numero_romano.append(letra_romana);
-            resto -= ROMANOS_A_DECIMAL.get(letra_romana);
+
+            /*Si meti dos letras*/
+            if (letra_romana.length() >= LETRAS_CONSECUTIVAS){
+                String letra_1 = letra_romana.substring(I_PRIMER_NUMERO, I_PRIMER_NUMERO+1);
+                String letra_2 = letra_romana.substring(I_SEGUNDO_NUMERO, I_SEGUNDO_NUMERO+1);
+                resto -= ROMANOS_A_DECIMAL.get(letra_2) - ROMANOS_A_DECIMAL.get(letra_1);
+            } else {
+                resto -= ROMANOS_A_DECIMAL.get(letra_romana);
+            }
         }
 
         return numero_romano.toString();
