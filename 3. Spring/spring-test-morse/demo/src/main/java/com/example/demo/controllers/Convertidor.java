59acd1c730f548a53752e343fc454676a85c530a
Juanil22 <juanilopez@santandertecnologia.com.ar>
2022-04-25 17:24:51 -0300

    improve

--- 3. Spring/spring-test-morse/demo/src/main/java/com/example/demo/controllers/Convertidor.java
+++ 3. Spring/spring-test-morse/demo/src/main/java/com/example/demo/controllers/Convertidor.java
@@ -15,41 +15,41 @@ public class Convertidor implements Buscador<List<String>> {
 
     @GetMapping(path = "/morse/{string}")
     public String conversionRomana(@PathVariable String string) {
-        HashMap letras = new HashMap();
-        letras.put(".-", "A");
-        letras.put("-...", "B");
-        letras.put("-.-.", "C");
-        letras.put("----", "CH");
-        letras.put("-..", "D");
-        letras.put(".", "E");
-        letras.put("..-.", "F");
-        letras.put("--.", "G");
-        letras.put("....", "H");
-        letras.put("..", "I");
-        letras.put(".---", "J");
-        letras.put("-.-", "K");
-        letras.put(".-..", "L");
-        letras.put("--", "M");
-        letras.put("-.", "N");
-        letras.put("--.--", "Ñ");
-        letras.put("---", "O");
-        letras.put(".--.", "P");
-        letras.put("--.-", "Q");
-        letras.put(".-.", "R");
-        letras.put("...", "S");
-        letras.put("-", "T");
-        letras.put("..-", "U");
-        letras.put("...-", "V");
-        letras.put(".--", "W");
-        letras.put("-..-","X");
-        letras.put("-.--","Y");
-        letras.put("--..", "Z");
+        HashMap mapa = new HashMap();
+        mapa.put(".-", "A");
+        mapa.put("-...", "B");
+        mapa.put("-.-.", "C");
+        mapa.put("----", "CH");
+        mapa.put("-..", "D");
+        mapa.put(".", "E");
+        mapa.put("..-.", "F");
+        mapa.put("--.", "G");
+        mapa.put("....", "H");
+        mapa.put("..", "I");
+        mapa.put(".---", "J");
+        mapa.put("-.-", "K");
+        mapa.put(".-..", "L");
+        mapa.put("--", "M");
+        mapa.put("-.", "N");
+        mapa.put("--.--", "Ñ");
+        mapa.put("---", "O");
+        mapa.put(".--.", "P");
+        mapa.put("--.-", "Q");
+        mapa.put(".-.", "R");
+        mapa.put("...", "S");
+        mapa.put("-", "T");
+        mapa.put("..-", "U");
+        mapa.put("...-", "V");
+        mapa.put(".--", "W");
+        mapa.put("-..-","X");
+        mapa.put("-.--","Y");
+        mapa.put("--..", "Z");
         String textoTraducido = "";
-        String[] palabras = string.split("   ");
-        for(String palabra : palabras) {
-            String[] letras2 = palabra.split(" ");
-            for(String caract : letras2) {
-                textoTraducido += letras.get(caract);
+        String[] palabrasMorse = string.split("   ");
+        for(String palabraMorse : palabrasMorse) {
+            String[] letrasPalabraMorse = palabraMorse.split(" ");
+            for(String letra : letrasPalabraMorse) {
+                textoTraducido += mapa.get(letra);
             }
             textoTraducido += " ";
         }
