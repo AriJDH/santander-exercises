7e067005b8b092173d22420b3fb627f241946cbb
deRivasLeandroSantander <lderivas@santandertecnologia.com.ar>
2022-04-25 16:11:29 -0300

    Soluciones

--- 3. Spring/ejercicio/ejercicio/src/main/java/com/ejercicio/ejercicio/Saludo.java
+++ 3. Spring/ejercicio/ejercicio/src/main/java/com/ejercicio/ejercicio/Saludo.java
@@ -3,12 +3,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.WeakHashMap;
+import java.util.stream.Collectors;
 
 @RestController
-public class Saludo {
+public class Saludo implements buscador<List<String>> {
     @GetMapping
     public String saludar() {
         return "Hola mundo, como va?";
@@ -119,5 +121,31 @@ public class Saludo {
             textoTraducido += " ";
         }
         return textoTraducido;
+    }
+
+    @Override
+    @GetMapping(path = "/buscarEnArreglo/{palabra}")
+    public List<String> buscarString(@PathVariable String palabra) {
+        List<String> memoria = new ArrayList<>(4);
+        memoria.add("HOLA");
+        memoria.add("HOLA");
+        memoria.add("HOLA");
+        memoria.add("CHAU");
+        memoria.add("NOS");
+        memoria.add("VEMOS");
+        List<String> res = memoria.stream().filter(p -> p.equals(palabra)).collect(Collectors.toList());
+        return res;
+    }
+
+    @GetMapping(path = "/buscarEnArregloBool/{palabra}")
+    public boolean buscarStringBool(@PathVariable String palabra) {
+        List<String> memoria = new ArrayList<>(4);
+        memoria.add("HOLA");
+        memoria.add("HOLA");
+        memoria.add("HOLA");
+        memoria.add("CHAU");
+        memoria.add("NOS");
+        memoria.add("VEMOS");
+        return memoria.stream().anyMatch(p -> p.equals(palabra));
     }
 }
---
+++ 3. Spring/ejercicio/ejercicio/src/main/java/com/ejercicio/ejercicio/buscador.java
@@ -0,0 +1,6 @@
+package com.ejercicio.ejercicio;
+
+public interface buscador<T> {
+
+    public T buscarString(String palabra);
+}
