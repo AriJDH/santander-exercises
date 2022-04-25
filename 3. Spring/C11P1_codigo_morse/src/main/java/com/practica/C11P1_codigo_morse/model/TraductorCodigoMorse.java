1580cf17974c51e7128c2821dea18332e62846c5
AgustinLeguizamon-ST <agleguizamon@santandertecnologia.com.ar>
2022-04-25 16:51:46 -0300

    fix traductor no agregaba espacios

--- 3. Spring/C11P1_codigo_morse/src/main/java/com/practica/C11P1_codigo_morse/model/TraductorCodigoMorse.java
+++ 3. Spring/C11P1_codigo_morse/src/main/java/com/practica/C11P1_codigo_morse/model/TraductorCodigoMorse.java
@@ -30,13 +30,17 @@ public class TraductorCodigoMorse {
 
     public String morse_a_castellano(String codigo_morse) {
         StringBuilder palabra = new StringBuilder("");
-        String[] letras_en_morse = codigo_morse.split(" ");
-        for (String letra_en_morse : letras_en_morse) {
-            if (MORSE_A_CASTELLANO.containsKey(letra_en_morse)) {
-                palabra.append(MORSE_A_CASTELLANO.get(letra_en_morse));
+        String[] palabras_en_morse = codigo_morse.split("   ");
+        for (String palabra_en_morse : palabras_en_morse) {
+            String[] letras_en_morse = palabra_en_morse.split(" ");
+            for (String letra_en_morse : letras_en_morse) {
+                if (MORSE_A_CASTELLANO.containsKey(letra_en_morse)) {
+                    palabra.append(MORSE_A_CASTELLANO.get(letra_en_morse));
+                }
             }
+            palabra.append(" ");
         }
 
-        return palabra.toString();
+        return palabra.substring(0, palabra.length()-1);
     }
 }
