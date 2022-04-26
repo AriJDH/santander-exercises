95ec176153c5cfd0becf26722ab0027bbedde4fe
deRivasLeandroSantander <lderivas@santandertecnologia.com.ar>
2022-04-26 09:27:29 -0300

    Ejercicio 2

--- 3. Spring/JSON_Spring/practica_spring_jason/src/main/java/com/santander/practica_spring_jason/HelloWorld.java
+++ 3. Spring/JSON_Spring/practica_spring_jason/src/main/java/com/santander/practica_spring_jason/HelloWorld.java
@@ -2,13 +2,14 @@ package com.santander.practica_spring_jason;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
-    @GetMapping(path = "/hello")
-    public String saludar() {
+    @GetMapping(path = "/hello/{name}")
+    public String saludar(@PathVariable String name) {
         Controller controller = new Controller();
-        return controller.saludar();
+        return controller.saludar() + " " + name;
     }
 }
