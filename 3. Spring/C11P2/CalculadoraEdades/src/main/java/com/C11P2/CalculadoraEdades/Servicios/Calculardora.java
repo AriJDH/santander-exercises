2c4df82ff5cb02790228d837f4aac5479d38ebf5
mcartelli <mcartellifernandez@santandertecnologia.com.ar>
2022-04-26 12:31:04 -0300

    Hecha función para calculo de edad

---
+++ 3. Spring/C11P2/CalculadoraEdades/src/main/java/com/C11P2/CalculadoraEdades/Servicios/Calculardora.java
@@ -0,0 +1,13 @@
+package com.C11P2.CalculadoraEdades.Servicios;
+
+import java.nio.channels.UnsupportedAddressTypeException;
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Calculardora {
+
+public int getEdad(int dias,int meses,int años){
+    Period Edad = Period.between(LocalDate.of(años, meses, dias),LocalDate.now());
+    return Edad.getYears();
+}
+}
