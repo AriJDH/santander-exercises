d2386559c8a28ed6755c081a4043123d7889c443
AgustinLeguizamon-ST <agleguizamon@santandertecnologia.com.ar>
2022-04-26 09:58:43 -0300

    agregadas carpetas teoria y practica clase C11P2

---
+++ 3. Spring/C11P2/src/main/java/com/clase/C11P2/C11P2Application.java
@@ -0,0 +1,22 @@
+package com.clase.C11P2;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C11P2Application implements CommandLineRunner {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(C11P2Application.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(C11P2Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		LOGGER.info("Hola Spring");
+	}
+}
---
+++ 3. Spring/C11P2Practica/src/test/java/com/clase/C11P2Practica/C11P2PracticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clase.C11P2Practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C11P2PracticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
