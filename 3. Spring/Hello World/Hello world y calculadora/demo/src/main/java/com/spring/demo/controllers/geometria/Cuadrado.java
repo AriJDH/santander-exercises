2ff3f6a0aa8a1aa7b5178bae7ed5f1b095bf82e8
gVersionFun <A446491@santandertecnologia.com.ar>
2022-04-26 13:40:31 -0300

    definiendo jerarquias

---
+++ 3. Spring/Hello World/Hello world y calculadora/demo/src/main/java/com/spring/demo/controllers/geometria/Cuadrado.java
@@ -0,0 +1,9 @@
+package com.spring.demo.controllers.geometria;
+
+public class Cuadrado extends Paralelogramo{
+
+    @Override
+    public double area(int base, int altura) {
+        return 0;
+    }
+}
---
+++ 3. Spring/Hello World/Hello world y calculadora/demo/src/main/java/com/spring/demo/controllers/geometria/Paralelogramo.java
@@ -0,0 +1,8 @@
+package com.spring.demo.controllers.geometria;
+
+public abstract class Paralelogramo extends Cuadrilatero {
+
+    public Paralelogramo(String name, int base, int altura) {
+        super(name, base, altura);
+    }
+}
