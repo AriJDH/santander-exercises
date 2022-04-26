0092e850500772310c6fcd323a8e385646d00246
mcartelli <mcartellifernandez@santandertecnologia.com.ar>
2022-04-26 17:01:15 -0300

    comienzo ejercicio Diploma C12P1

---
+++ 3. Spring/C12P1/Diploma/src/main/java/com/C12P1/Diploma/DiplomaApplication.java
@@ -0,0 +1,13 @@
+package com.C12P1.Diploma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiplomaApplication.class, args);
+	}
+
+}
---
+++ 3. Spring/C12P1/Diploma/src/test/java/com/C12P1/Diploma/DiplomaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.C12P1.Diploma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
