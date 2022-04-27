65865e55d67d808afae77026cd12343c5fd02621
anggiemiranda <103551076+anggiemiranda@users.noreply.github.com>
2022-04-27 20:07:51 -0300

    Delete 3. Spring/C13-Excepciones-EjercicioStarWars directory

--- 3. Spring/C13-Excepciones-EjercicioStarWars/src/main/java/com/santander/C13ExcepcionesEjercicioStarWars/repo/Dao.java
+++
@@ -1,9 +0,0 @@
-package com.santander.C13ExcepcionesEjercicioStarWars.repo;
-
-import com.santander.C13ExcepcionesEjercicioStarWars.modelos.Personaje;
-
-import java.util.List;
-
-public interface Dao <K,E> {
-    public List<E> getElementos(K clave);
-}
