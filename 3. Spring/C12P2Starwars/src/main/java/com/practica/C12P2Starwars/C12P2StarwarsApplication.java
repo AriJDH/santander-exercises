2bd8caed81587b6929d237e1084df9ab80c87bbc
AgustinLeguizamon-ST <agleguizamon@santandertecnologia.com.ar>
2022-04-27 16:12:05 -0300

    agregada carpeta ejercicio star wars

---
+++ 3. Spring/C12P2Starwars/src/main/java/com/practica/C12P2Starwars/C12P2StarwarsApplication.java
@@ -0,0 +1,13 @@
+package com.practica.C12P2Starwars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C12P2StarwarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C12P2StarwarsApplication.class, args);
+	}
+
+}
---
+++ 3. Spring/C12P2Starwars/src/test/java/com/practica/C12P2Starwars/C12P2StarwarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.C12P2Starwars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C12P2StarwarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
