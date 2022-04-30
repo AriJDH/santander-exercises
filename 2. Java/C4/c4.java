df4294a33921d823aa7318fd289465353a154ef7
florenciarganaraz <flarganaraz@santandertecnologia.com.ar>
2022-04-30 14:58:19 -0300

    ejercicios1 y 2,intento1

---
+++ 2. Java/C4/c4.java
@@ -0,0 +1,35 @@
+public class c4 {
+
+    //EJERCICIO 1//-----------------------------------------
+    //Consigna//
+    /*String apellido = Gomez;
+    int edad = "35";
+    boolean = "false";
+    String sueldo = 45857.90
+    double nombre = "Julián";*/
+
+    String apellido = "Gomez";
+    int edad = 35;
+    boolean esMayorDeEdad = false;
+    float sueldo = 45857.90F;
+    String nombre ="Julian";
+
+    //EJERCICIO 2//--------------------------------------
+    //CONSIGNA
+    /*String 1nombre;
+    int edad;
+    double $ueldo;
+    String @pellido;
+    String direccion;
+    boolean licencia_de_conducir;
+    double estatura de la persona;
+    int cantidad-de-hijos;
+    */
+
+    String nombre1; //No pueden comenzar con numeros
+    double $ueldo; // No pueden comenzar con caracteres especiales
+    String apellido1; //No pueden comenzar con caracteres especiales
+    double estaturaDeLaPersona;
+
+
+}
