1caf42a3ec88dfef6386ffe2a697e763e838b6a8
mcartelli <mcartellifernandez@santandertecnologia.com.ar>
2022-05-05 11:27:54 -0300

    Terminado Validaciones de StudentDto y SubjectDto

--- 5. Testing/C15P2/ObtenerDiploma/src/main/java/com/digitalhouse/obtenerdiploma/model/SubjectDTO.java
+++ 5. Testing/C15P2/ObtenerDiploma/src/main/java/com/digitalhouse/obtenerdiploma/model/SubjectDTO.java
@@ -3,8 +3,18 @@ package com.digitalhouse.obtenerdiploma.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.*;
+
 @Getter @Setter
 public class SubjectDTO {
+
+    @NotEmpty(message = "El nombre de la materia no puede estar vacío.")
+    @Pattern(regexp = "[A-Z](\\p{Alpha}||\\s)+", message = "El nombre de la materia debe comenzar con mayúscula.")
+    @Size(max = 30,message = "La longitud del nombre no puede superar los 30 caracteres.")
     String name;
+
+    @NotEmpty(message = "La nota no puede estar vacío.")
+    @DecimalMin("0.0")
+    @DecimalMax("10")
     Double score;
 }
