84a82f2a1bb4caa6bfc39b2fdc3da97e92e2a0de
AgustinLeguizamon-ST <agleguizamon@santandertecnologia.com.ar>
2022-05-05 11:05:30 -0300

    agregadas validaciones a SubjectDTO

--- 5. Testing/C15P2ObtenerDiploma/src/main/java/com/digitalhouse/obtenerdiploma/model/StudentDTO.java
+++ 5. Testing/C15P2ObtenerDiploma/src/main/java/com/digitalhouse/obtenerdiploma/model/StudentDTO.java
@@ -3,6 +3,7 @@ package com.digitalhouse.obtenerdiploma.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
@@ -18,5 +19,5 @@ public class StudentDTO {
     String message;
     Double averageScore;
     @NotEmpty(message = "La lista no puede ser vacía.")
-    List<SubjectDTO> subjects;
+    List<@Valid SubjectDTO> subjects;
 }
--- 5. Testing/C15P2ObtenerDiploma/src/main/java/com/digitalhouse/obtenerdiploma/model/SubjectDTO.java
+++ 5. Testing/C15P2ObtenerDiploma/src/main/java/com/digitalhouse/obtenerdiploma/model/SubjectDTO.java
@@ -3,8 +3,19 @@ package com.digitalhouse.obtenerdiploma.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.*;
+
 @Getter @Setter
 public class SubjectDTO {
+    @NotNull
+    @NotEmpty(message = "El nombre de la materia no puede estar vacía.")
+    //TODO: problemas con las tildes
+    @Pattern(regexp = "[A-Z](\\p{Alpha}||\\s)+", message = "El nombre de la materia debe comenzar con mayúscula.")
+    @Size(max = 30, message = "La longitud del nombre no puede superar los 30 caracteres.")
     String name;
+
+    @NotNull(message = "La nota no puede estar vacía")
+    @Min(value = 0, message = "La minima nota es 0.0.")
+    @Max(value = 10, message = "La máxima nota es 10.0.")
     Double score;
 }
