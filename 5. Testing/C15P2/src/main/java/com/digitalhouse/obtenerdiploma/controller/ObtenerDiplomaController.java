62a594011d2f13b1d92b34a9aa130473fd00a59c
rubencasco <rcasco@santandertecnologia.com.ar>
2022-05-05 11:06:58 -0300

    resuelto ejercicio 1 de C15P2

---
+++ 5. Testing/C15P2/src/main/java/com/digitalhouse/obtenerdiploma/controller/ObtenerDiplomaController.java
@@ -0,0 +1,19 @@
+package com.digitalhouse.obtenerdiploma.controller;
+
+import com.digitalhouse.obtenerdiploma.model.StudentDTO;
+import com.digitalhouse.obtenerdiploma.service.IObtenerDiplomaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+public class ObtenerDiplomaController {
+    @Autowired
+    IObtenerDiplomaService service;
+
+    @PostMapping("/analyzeScores")
+    public StudentDTO analyzeScores(@Valid @RequestBody StudentDTO rq) {
+        return service.analyzeScores(rq);
+    }
+}
---
+++ 5. Testing/C15P2/src/main/java/com/digitalhouse/obtenerdiploma/model/StudentDTO.java
@@ -0,0 +1,24 @@
+package com.digitalhouse.obtenerdiploma.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Getter @Setter
+public class StudentDTO {
+    @NotBlank(message = "El nombre del alumno no puede estar vacío.")
+    @Size(max = 50, message = "La longitud del nombre no puede superar los 50 caracteres.")
+    @Pattern(regexp = "[A-Z](\\p{Alpha}||\\s)+", message = "El nombre del alumno debe comenzar con mayúscula.")
+    String studentName;
+    String message;
+    Double averageScore;
+    @NotEmpty(message = "La lista no puede ser vacía.")
+    List<@Valid SubjectDTO>  subjects;
+}
