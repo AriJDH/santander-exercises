75f2893081a1af4b8b84a7a27530fcbc77d68600
AgustinLeguizamon-ST <agleguizamon@santandertecnologia.com.ar>
2022-05-05 15:39:46 -0300

    agregada demo hecha en clases

---
+++ 5. Testing/demo/src/main/java/com/example/demo/servicios/Calculadora.java
@@ -0,0 +1,12 @@
+package com.example.demo.servicios;
+
+public class Calculadora {
+
+    public void calcular() {
+        int a = 10/0;
+    }
+
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+}
