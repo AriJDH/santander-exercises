31445ee11d584fe0a8ab784107ec654511bcab81
AgustinLeguizamon-ST <agleguizamon@santandertecnologia.com.ar>
2022-05-05 16:01:53 -0300

    agregado test a repo de cliente

---
+++ 5. Testing/demo/src/test/java/com/example/demo/repo/ClienteRepoTest.java
@@ -0,0 +1,37 @@
+package com.example.demo.repo;
+
+import com.example.demo.modelos.Cliente;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.beans.BeanProperty;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClienteRepoTest {
+    private ClienteRepo clienteRepo;
+
+    @BeforeEach
+    void setup(){
+        clienteRepo = new ClienteRepo();
+    }
+
+    @Test
+    void getCliente() {
+        //Arrange
+        Cliente clienteAGuardar = new Cliente("Juan", "Carlos", LocalDate.of(1995,3,23));
+
+        //Act
+        clienteRepo.putCliente(clienteAGuardar);
+        Cliente clienteObtenido = clienteRepo.getCliente();
+
+        //Asserts
+        assertEquals(clienteAGuardar, clienteObtenido);
+    }
+
+    @Test
+    void putCliente() {
+
+    }
+}
