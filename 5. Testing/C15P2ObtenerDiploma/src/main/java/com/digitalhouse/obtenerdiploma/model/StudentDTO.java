9055130a2bc50ee68168eb7d6e54014c1afd2fab
AgustinLeguizamon-ST <agleguizamon@santandertecnologia.com.ar>
2022-05-05 10:51:56 -0300

    agregadas validaciones a StudentDTO

--- 5. Testing/C15P2ObtenerDiploma/src/main/java/com/digitalhouse/obtenerdiploma/model/StudentDTO.java
+++ 5. Testing/C15P2ObtenerDiploma/src/main/java/com/digitalhouse/obtenerdiploma/model/StudentDTO.java
@@ -3,12 +3,20 @@ package com.digitalhouse.obtenerdiploma.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Getter @Setter
 public class StudentDTO {
+    @Size(max = 50, message = "La longitud del nombre no puede superar los 50 caracteres")
+    @NotNull(message = "El nombre del alumno no puede estar vacio")
+    @Pattern(regexp = "[A-Z](\\p{Alpha}||\\s)+", message = "El nombre del estudiante debe comenzar con mayúscula.")
     String studentName;
     String message;
     Double averageScore;
+    @NotEmpty(message = "La lista no puede ser vacía.")
     List<SubjectDTO> subjects;
 }
