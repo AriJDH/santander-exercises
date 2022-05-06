3a66c49e319481539d3802ae4175ed5e5aa10346
MarianaRodriguez1 <marianarodriguez1@santandertecnologia.com.ar>
2022-05-06 17:54:08 -0300

    test de integracion getListStudents

--- 5. Testing/C17P1 - Test de integración/src/test/java/com/practica/obtenerdiploma/controller/StudentControllerIntegrationTests.java
+++ 5. Testing/C17P1 - Test de integración/src/test/java/com/practica/obtenerdiploma/controller/StudentControllerIntegrationTests.java
@@ -1,8 +1,12 @@
 package com.practica.obtenerdiploma.controller;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.practica.obtenerdiploma.model.StudentDTO;
+import com.practica.obtenerdiploma.util.TestUtilsGenerator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.asm.TypeReference;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +15,9 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -26,6 +33,11 @@ public class StudentControllerIntegrationTests {
 
     @Test
     public void testGetStudent() throws Exception{
+        TestUtilsGenerator.emptyUsersFile();
+        StudentDTO studentDTO = TestUtilsGenerator.getStudentWithId(1L);
+        studentDTO.setStudentName("Juan");
+        TestUtilsGenerator.appendNewStudent(studentDTO);
+
         MvcResult mvcResult =
                 this.mockMvc.perform(MockMvcRequestBuilders.get("/student/getStudent/{id}",1))
                         .andDo(print()).andExpect(status().isOk())
@@ -36,5 +48,39 @@ public class StudentControllerIntegrationTests {
                         .andReturn();
         Assertions.assertEquals("application/json",mvcResult.getResponse().getContentType());
 
+
     }
+
+    @Test
+    public void testListStudentsWhenHasOneElement() throws Exception{
+        TestUtilsGenerator.emptyUsersFile();
+        StudentDTO studentDTO = TestUtilsGenerator.getStudentWithId(1L);
+        studentDTO.setStudentName("Juan");
+        TestUtilsGenerator.appendNewStudent(studentDTO);
+
+        MvcResult mvcResult =
+                this.mockMvc.perform(MockMvcRequestBuilders.get("/student/listStudents"))
+                        .andDo(print()).andExpect(status().isOk())
+                        .andExpect(content().contentType("application/json"))
+                        .andExpect(MockMvcResultMatchers.jsonPath("$[0].studentName").value("Juan"))
+                        .andReturn();
+        Assertions.assertEquals("application/json",mvcResult.getResponse().getContentType());
+
+    }
+
+    @Test
+    public void testListStudentsWhenHasntElements() throws Exception{
+        TestUtilsGenerator.emptyUsersFile();
+        MvcResult mvcResult =
+                this.mockMvc.perform(MockMvcRequestBuilders.get("/student/listStudents"))
+                        .andDo(print()).andExpect(status().isOk())
+                        .andExpect(content().contentType("application/json"))
+                        .andReturn();
+        String response = mvcResult.getResponse().getContentAsString();
+        Assertions.assertEquals("application/json",mvcResult.getResponse().getContentType());
+        Assertions.assertEquals("[]",response);
+
+    }
+
+
 }
