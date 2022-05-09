1a3055306e3771362db2274df6895bad99d8f5fe
AgustinLeguizamon-ST <agleguizamon@santandertecnologia.com.ar>
2022-05-09 09:26:05 -0300

    agregado ejercicio test de integracion

---
+++ 5. Testing/C17P1 - Test de integracion/src/test/java/com/practica/obtenerdiploma/repository/StudentRepositoryTests.java
@@ -0,0 +1,36 @@
+package com.practica.obtenerdiploma.repository;
+
+import com.practica.obtenerdiploma.model.StudentDTO;
+import com.practica.obtenerdiploma.util.TestUtilsGenerator;
+import org.apache.commons.collections4.CollectionUtils;
+import org.junit.jupiter.api.*;
+
+import java.util.Set;
+
+public class StudentRepositoryTests {
+
+    IStudentRepository studentRepo;
+    IStudentDAO studentDAO;
+
+    @BeforeEach @AfterEach
+    private void setUp() {
+        TestUtilsGenerator.emptyUsersFile();
+
+        this.studentDAO = new StudentDAO();
+        this.studentRepo = new StudentRepository();
+    }
+
+    @Test
+    public void findAllExistentStudents() {
+        // arrange
+        Set<StudentDTO> students = TestUtilsGenerator.getStudentSet();
+        students.forEach((stu) -> studentDAO.save(stu));
+
+        // act
+        Set<StudentDTO> foundSet = studentRepo.findAll();
+
+        // assert
+        Assertions.assertTrue(CollectionUtils.isEqualCollection(students, foundSet));
+    }
+
+}
