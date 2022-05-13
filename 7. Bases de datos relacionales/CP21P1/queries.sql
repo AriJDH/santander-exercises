2fa202fe8c670089f3d2da8a9eb2e073a2bc08d5
AgustinLeguizamon-ST <agleguizamon@santandertecnologia.com.ar>
2022-05-13 18:02:22 -0300

    agregado enunciado, modelo, script para levantar db y script queries

---
+++ 7. Bases de datos relacionales/CP21P1/queries.sql
@@ -0,0 +1,12 @@
+
+-- 1. Listar los datos de los autores.
+SELECT * FROM autor;
+
+-- 2. Listar nombre y edad de los estudiantes.
+SELECT * FROM estudiante;
+
+SELECT * FROM libro;
+
+SELECT * FROM libro_autor;
+
+SELECT * FROM prestamo;
