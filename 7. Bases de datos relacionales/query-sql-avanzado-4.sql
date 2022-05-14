9a44832923787e17d13e21d63e82afa8af5251aa
Juanil22 <juanilopez@santandertecnologia.com.ar>
2022-05-14 01:13:33 -0300

    fixing

--- 7. Bases de datos relacionales/query-sql-avanzado-4.sql
+++ 7. Bases de datos relacionales/query-sql-avanzado-4.sql
@@ -12,7 +12,7 @@ SELECT * FROM  libro WHERE Editorial="papi";
 
 SELECT * FROM  estudiante WHERE edad> (SELECT AVG(edad) FROM estudiante);
 
-SELECT * FROM estudiante WHERE apellido LIKE "g%";
+SELECT nombre FROM estudiante WHERE apellido LIKE "g%";
 
 SELECT autor.nombre FROM autor INNER JOIN libroautor ON autor.idAutor = libroautor.idAutor INNER JOIN libro ON libro.idLibro = libroautor.idLibro WHERE libro.Titulo="HP1";
 
@@ -20,9 +20,9 @@ SELECT libro.Titulo FROM libro INNER JOIN prestamo ON libro.idLibro = prestamo.i
 
 SELECT min(edad) as minimaEdad, nombre FROM estudiante;
 
-SELECT estudiante.nombre FROM libro INNER JOIN prestamo ON libro.idLibro = prestamo.idLibro INNER JOIN estudiante ON prestamo.idLector = estudiante.idLector WHERE libro.Area="fantasia";
+SELECT distinct estudiante.nombre FROM libro INNER JOIN prestamo ON libro.idLibro = prestamo.idLibro INNER JOIN estudiante ON prestamo.idLector = estudiante.idLector WHERE libro.Area="fantasia";
 
 SELECT libro.* FROM autor INNER JOIN libroautor ON autor.idAutor = libroautor.idAutor INNER JOIN libro ON libro.idLibro = libroautor.idLibro WHERE autor.nombre="juani";
 
-SELECT libro.Titulo FROM libro INNER JOIN prestamo ON libro.idLibro = prestamo.idLibro WHERE prestamo.FechaDevolucion="2020-02-04";
+SELECT distinct libro.Titulo FROM libro INNER JOIN prestamo ON libro.idLibro = prestamo.idLibro WHERE prestamo.FechaDevolucion="2020-02-04";
 
