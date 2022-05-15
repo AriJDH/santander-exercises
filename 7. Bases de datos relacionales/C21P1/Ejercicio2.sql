778487694b77694c50a90ca23d8d7e1380ec4eda
mcartelli <mcartellifernandez@santandertecnologia.com.ar>
2022-05-15 20:24:22 -0300

    Terminado ejercicio 1 C21P1

---
+++ 7. Bases de datos relacionales/C21P1/Ejercicio2.sql
@@ -0,0 +1,15 @@
+-- ¿A qué se denomina JOIN en una base de datos y para qué se utiliza?
+-- A combinar datos de diferentes tablas a partir de condiciones en común.
+
+-- Explicar dos tipos de JOIN.
+-- El inner join toma la intersección de ambas tablas, el left join tomará la intersección entre dos tablas pero seguirá
+--  mostrando todos los datos correspondientes de la tabla izquierda aún cuando en la tabla derecha halla un valor null.
+-- El right join es lo mismo pero prioriza a la tabla de la derecha. Por último el full outer join es la unión entre tablas.
+
+-- ¿Para qué se utiliza el GROUP BY?
+-- se utiliza para obtener datos resumidos y agrupados por las columnas correspondientes ya que genera un sólo registro por un grupo 
+-- de filas que compartan el mismo valor en la columna.
+
+-- ¿Para qué se utiliza el HAVING?
+-- Funciona como el WHERE pero para las columnas obtenidas mediante el GROUP BY.
+
