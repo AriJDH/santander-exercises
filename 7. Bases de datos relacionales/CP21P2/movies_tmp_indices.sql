68ea86c1201b88dc291402bdf38ea5c8b4b03bf7
AgustinLeguizamon-ST <agleguizamon@santandertecnologia.com.ar>
2022-05-16 17:36:05 -0300

    agregada consulta sobre tabla tmp

--- 7. Bases de datos relacionales/CP21P2/movies_tmp_indices.sql
+++ 7. Bases de datos relacionales/CP21P2/movies_tmp_indices.sql
@@ -1,15 +1,20 @@
 -- Creo tabla temporal
+DROP TABLE IF EXISTS `TWD`;
 CREATE TEMPORARY TABLE TWD
 SELECT * FROM episodes
 WHERE season_id IN
-	(SELECT * 
+	(SELECT id 
     FROM seasons
     WHERE serie_id IN
 		(SELECT id 
         FROM series
         WHERE title = 'The Walking Dead')
-    )
-;
+    );
 
---
-SELECT * FROM TWD
+-- Episodios primera temporada
+SELECT * 
+FROM TWD
+WHERE season_id = 20
+
+-- Creamos un indice
+-- CREATE INDEX 
