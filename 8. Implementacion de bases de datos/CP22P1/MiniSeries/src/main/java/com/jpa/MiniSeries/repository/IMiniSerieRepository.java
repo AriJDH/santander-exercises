a21517bfe9806561fb0a65032e805d554a0bd0fd
rubencasco <rcasco@santandertecnologia.com.ar>
2022-05-17 12:07:26 -0300

    resuelto ejercicio de CP22P1

---
+++ 8. Implementacion de bases de datos/CP22P1/MiniSeries/src/main/java/com/jpa/MiniSeries/repository/IMiniSerieRepository.java
@@ -0,0 +1,9 @@
+package com.jpa.MiniSeries.repository;
+
+import com.jpa.MiniSeries.model.MiniSerie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IMiniSerieRepository extends JpaRepository<MiniSerie,Long> {
+}
