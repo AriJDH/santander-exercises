ae38f43de42031df44cf187ac27fa5cf39cc57cf
MarianaRodriguez1 <marianarodriguez1@santandertecnologia.com.ar>
2022-05-17 11:59:13 -0300

    JPA MiniSeries

---
+++ 8. Implementacion de bases de datos/CP22P1 - JPA MiniSeries/MiniSeries/src/main/java/com/jpa/MiniSeries/model/MiniSerie.java
@@ -0,0 +1,26 @@
+package com.jpa.MiniSeries.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "miniseries")
+public class MiniSerie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private Double rating;
+
+    private int amount_of_awards;
+}
