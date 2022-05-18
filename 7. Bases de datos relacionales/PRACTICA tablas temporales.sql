86053358aa70ce474d553a0b20e6376233b38c6e
florenciarganaraz <103457733+florenciarganaraz@users.noreply.github.com>
2022-05-18 19:15:02 -0300

    Add files via upload

---
+++ 7. Bases de datos relacionales/PRACTICA tablas temporales.sql
@@ -0,0 +1,21 @@
+
+-- Ejercicio 1
+SELECT id FROM seasons WHERE serie_id =3;
+
+SELECT * FROM series;
+
+SELECT * FROM episodes;
+DROP TABLE twd;
+CREATE TEMPORARY TABLE twd 
+SELECT * FROM episodes WHERE season_id IN (SELECT id FROM seasons WHERE serie_id = 3);
+
+SELECT * FROM twd;
+
+SELECT * FROM twd WHERE season_id = 20; 
+
+-- Ejercicio 2
+
+CREATE INDEX rating_index ON movies(rating); 
+
+
+SHOW INDEX FROM movies; 
