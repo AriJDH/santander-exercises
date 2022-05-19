9a517d16538bad4de8454cec99f2751f41474641
AgustinLeguizamon-ST <agleguizamon@santandertecnologia.com.ar>
2022-05-19 15:07:15 -0300

    se removio carpeta school desactualizada

--- 8. Implementacion de bases de datos/CP22P1/school/src/main/java/com/example/school/repository/StudentsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.school.repository;
-
-import com.example.school.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentsRepository extends JpaRepository<Student, Integer> {
-}
--- 8. Implementacion de bases de datos/CP22P1/school/src/main/java/com/example/school/repository/SubjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.school.repository;
-
-import com.example.school.entity.Subject;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubjectRepository extends JpaRepository<Subject, Integer> {
-}
