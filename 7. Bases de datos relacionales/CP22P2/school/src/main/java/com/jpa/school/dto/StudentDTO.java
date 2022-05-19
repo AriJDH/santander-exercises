cda32c27019258dc310918a8036a4c3ffb223c7b
AgustinLeguizamon-ST <agleguizamon@santandertecnologia.com.ar>
2022-05-19 12:06:33 -0300

    agregado courseId a studentdto

--- 7. Bases de datos relacionales/CP22P2/school/src/main/java/com/jpa/school/dto/StudentDTO.java
+++ 7. Bases de datos relacionales/CP22P2/school/src/main/java/com/jpa/school/dto/StudentDTO.java
@@ -1,5 +1,6 @@
 package com.jpa.school.dto;
 
+import com.jpa.school.repository.CourseRepository;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,4 +12,7 @@ public class StudentDTO {
     String dni;
     String name;
     Boolean disabled;
+    /*o le paso el id o le paso el curseDTOo*/
+    Integer courseId;
+    //CourseRepository course;
 }
