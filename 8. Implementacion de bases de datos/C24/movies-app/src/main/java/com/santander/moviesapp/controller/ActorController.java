2560f483e33aebe4ee48010e99b924fe2713cfbf
AgustinLeguizamon-ST <agleguizamon@santandertecnologia.com.ar>
2022-05-20 12:44:49 -0300

    agregado endpoint get all actors

--- 8. Implementacion de bases de datos/C24/movies-app/src/main/java/com/santander/moviesapp/controller/ActorController.java
+++ 8. Implementacion de bases de datos/C24/movies-app/src/main/java/com/santander/moviesapp/controller/ActorController.java
@@ -1,9 +1,23 @@
 package com.santander.moviesapp.controller;
 
 
+import com.santander.moviesapp.dto.response.ActorResponseDto;
+import com.santander.moviesapp.service.ActorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ActorController {
 
+    @Autowired
+    ActorService actorsService;
+
+    @GetMapping("/actors")
+    public ResponseEntity<List<ActorResponseDto>> getAllActors(){
+        List<ActorResponseDto> actors = actorsService.getAllActors();
+        return ResponseEntity.ok().body(actors);
+    }
 }
--- 8. Implementacion de bases de datos/C24/movies-app/src/main/java/com/santander/moviesapp/service/ActorService.java
+++ 8. Implementacion de bases de datos/C24/movies-app/src/main/java/com/santander/moviesapp/service/ActorService.java
@@ -1,9 +1,14 @@
 package com.santander.moviesapp.service;
 
+import com.santander.moviesapp.dto.response.ActorResponseDto;
+import com.santander.moviesapp.entity.ActorEntity;
 import com.santander.moviesapp.repository.IActorRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ActorService{
@@ -12,5 +17,10 @@ public class ActorService{
 
     ModelMapper mapper = new ModelMapper();
 
-
+    public List<ActorResponseDto> getAllActors() {
+        List<ActorEntity> actors = actorRepository.findAll();
+        return actors.stream()
+                .map(actorEntity -> mapper.map(actorEntity, ActorResponseDto.class))
+                .collect(Collectors.toList());
+    }
 }
