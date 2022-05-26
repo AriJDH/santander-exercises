0ba75823cce6a0f371881a21b34994c7cd6b7570
MatiasRomeroWasTaken <matiasromero@santandertecnologia.com.ar>
2022-05-26 18:45:52 -0300

    Subiendo ejercicio C29

---
+++ 9. Spring Security/C29/src/main/java/com/jpa/integrador/config/JwtUtil.java
@@ -0,0 +1,108 @@
+package com.jpa.integrador.config;
+
+import com.jpa.integrador.entity.Role;
+import com.jpa.integrador.repository.RoleRepository;
+import com.jpa.integrador.repository.UserEntityRepository;
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.util.*;
+
+@Service
+public class JwtUtil {
+
+	private String secret;
+	private int jwtExpirationInMs;
+
+	@Autowired
+	RoleRepository roleRepository;
+
+	@Value("${jwt.secret}")
+	public void setSecret(String secret) {
+		this.secret = secret;
+	}
+
+	@Value("${jwt.expirationDateInMs}")
+	public void setJwtExpirationInMs(int jwtExpirationInMs) {
+		this.jwtExpirationInMs = jwtExpirationInMs;
+	}
+
+	public String generateToken(UserDetails userDetails) {
+		Map<String, Object> claims = new HashMap<>();
+
+		Collection<? extends GrantedAuthority> roles = userDetails.getAuthorities();
+
+		for (Role rol :
+				roleRepository.findAll()) {
+			if(roles.contains(new SimpleGrantedAuthority("ROLE_"+rol.getRole_name())))
+			{
+				String roleAux= StringUtils.capitalize(rol.getRole_name().toLowerCase());
+				claims.put("is"+roleAux,true);
+			}
+		}
+		return doGenerateToken(claims, userDetails.getUsername());
+	}
+
+	private String doGenerateToken(Map<String, Object> claims, String subject) {
+		return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+				.setExpiration(new Date(System.currentTimeMillis() + jwtExpirationInMs))
+				.signWith(SignatureAlgorithm.HS512, secret).compact();
+
+	}
+
+	public boolean validateToken(String authToken) {
+		try {
+			Jws<Claims> claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(authToken);
+			return true;
+		} catch (SignatureException | MalformedJwtException | UnsupportedJwtException | IllegalArgumentException ex) {
+			throw new BadCredentialsException("INVALID_CREDENTIALS", ex);
+		} catch (ExpiredJwtException ex) {
+			throw ex;
+		}
+	}
+
+	public String getUsernameFromToken(String token) {
+		Claims claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+		return claims.getSubject();
+
+	}
+
+	public List<SimpleGrantedAuthority> getRolesFromToken(String token) {
+		Claims claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+		List<SimpleGrantedAuthority> roles= new ArrayList<>();
+		List<Role> roleList = roleRepository.findAll();
+		for (Role role :
+				roleList) {
+			String roleAux= StringUtils.capitalize(role.getRole_name().toLowerCase());
+			Boolean aux= claims.get("is"+roleAux,Boolean.class);
+			//y nos ahorramos el != null
+			if (Boolean.TRUE.equals(aux)) {
+				roles.add(new SimpleGrantedAuthority("ROLE_"+role.getRole_name()));
+			}
+		}
+
+		/*
+		Boolean isAdmin = claims.get("isAdmin", Boolean.class);
+		Boolean isUser = claims.get("isUser", Boolean.class);
+
+		if (isAdmin != null && isAdmin) {
+			roles = Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"));
+		}
+
+		if (isUser != null && isUser) {
+			roles = Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
+		}
+
+		 */
+		return roles;
+
+	}
+
+}
