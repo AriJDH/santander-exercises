81a1d07c56d79ee5ab059e855f9cf0dd41b2f22b
facusalvia <fsalvia@santandertecnologia.com.ar>
2022-05-26 09:22:50 -0300

    ejercicios 24-05

---
+++ 9. Spring Security/repasoJPA/src/main/java/com/jpa/integrador/config/SecurityConfig.java
@@ -0,0 +1,6 @@
+package com.jpa.integrador.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
